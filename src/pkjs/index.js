c252dfe83db91ca2b4117764ad4b1af4b7eef14e
Lyle Morris <lylemorris@pm.me>
2026-08-27 15:33:42 -0400

    Use current Redux app config for QA

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,5 +1,5 @@
-var CONFIG_URL = 'https://lyle-morris.github.io/Hosting/apps/redux/qa/app-config-v8.html';
-var CONFIG_VERSION = 'redux-qa-v8';
+var CONFIG_URL = 'https://lyle-morris.github.io/Hosting/apps/redux/qa/app-config.html';
+var CONFIG_VERSION = 'redux-qa-v9';
 var SETTINGS_KEY = 'redux_qa_settings_v2';
 
 var DEFAULTS = {
